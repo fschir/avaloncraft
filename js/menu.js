857dd9eb68953747521274fc84506fe0e9b76c4c
fschir <sonnensturz@gmail.com>
2016-07-06 20:35:37 +0200

    worked on the side menu

--- js/menu.js
+++ js/menu.js
@@ -10,7 +10,6 @@ var main = function() {
         $('body').animate({
             left: '185px'
         }, 200);
-        $('body').css("overflow", "hidden");
     });
 
     $('.icon-close').click(function() {
@@ -22,7 +21,17 @@ var main = function() {
             left: '0px'
         }, 200);
     });
-    
+
+    $('.about').click(function(){
+        $('.center-text').text("Avaloncraft besteht bereits seit 2013. Das Projekt startete als kleiner Server für ein paar Freunde" +
+            "vor einigen Monaten haben wir dann beschlossen einen Root-Server zu mieten und unseren Server für alle anderen Spieler" +
+            "zu öffnen. Im moment ist der Server noch ein ziemlicher Rohbau während wir immer weiter an allen Kleinigkeiten wie Plugins" +
+            "Multiverse etc. feilen. Aber Ihr seid alle herzlich eingeladen unseren Server zu besuchen und uns dabei Gesellschaft zu leisten")
+    });
+    $('.contact').click(function() {
+        $('.center-text').text("Ihr könnt uns direkt auf dem Server oder auf TS erreichen.Alternativ avaloncraft@gmail.com");
+    });
+
 };
 
 
